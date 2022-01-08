dd938c9b5e48b372647548e55db4bd916d89660a
YuliyaFakih <yuliya.bandaruk@gmail.com>
2022-01-08 21:25:18 +0300

    dictionary design finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Route, Routes} from 'react-router-dom'
-import Homepage from './components/Homepage.jsx'
+import Homepage from './components/Homepage/Homepage.jsx'
+import WordToSearch from './components/WordToSearch/WordToSearch.jsx';
 
 function App() {
   return (
       <div className="App">
           <Routes>
             <Route path='/' element={<Homepage />} />
+          </Routes>
+          <Routes>
+            <Route path='/word-to-search' element={<WordToSearch />} />
           </Routes>
       </div>
   );
